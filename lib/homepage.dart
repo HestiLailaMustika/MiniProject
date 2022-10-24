d32bbf449cac550b8880eb48043ec02e5a371c39
Hesti <20200410700121@student.umj.ac.id>
2022-10-24 10:21:21 +0700

    first commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,50 +1,93 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:overlay_support/overlay_support.dart';
+import 'package:project_a/detail.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class HomePage extends StatelessWidget {
-  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+//import 'package:a/detail.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List _posts = [];
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("HomePage"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              alignment: Alignment.topCenter,
-              width: MediaQuery.of(context).size.width / 1.06,
-              height: 50.0,
-              color: Colors.cyanAccent,
-              child: Text(
-                "Welcome to homepage",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black87,
-                    fontSize: 25.0),
-              ),
+      appBar: AppBar(title: Text('Berita Terkini')),
+      body: ListView.builder(
+        itemCount: _posts.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Container(
+              color: Colors.grey[200],
+              height: 100,
+              width: 100,
+              child: _posts[index]['urlToImage'] != null
+                  ? Image.network(
+                      _posts[index]['urlToImage'],
+                      width: 100,
+                      fit: BoxFit.cover,
+                    )
+                  : Center(),
             ),
-            SizedBox(
-              height: 50,
+            title: Text(
+              '${_posts[index]['title']}',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
             ),
-            Container(
-                width: MediaQuery.of(context).size.width / 1.06,
-                child: ElevatedButton(
-                    onPressed: () async {
-                      final SharedPreferences prefs = await _prefs;
-                      prefs.setBool("isLoggedIn", false);
-                      showSimpleNotification(Text("You are logged Out"),
-                          background: Colors.red);
-                    },
-                    child: Text("Logout")))
-          ],
-        ),
+            subtitle: Text(
+              '${_posts[index]['description']}',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (c) => Detail(
+                    url: _posts[index]['url'],
+                    title: _posts[index]['title'],
+                    content: _posts[index]['context'],
+                    publishedAt: _posts[index]['publishedAt'],
+                    author: _posts[index]['author'],
+                    urlToImage: _posts[index][' urlToImage'],
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
+  }
+
+  Future _getData() async {
+    try {
+      final response = await http.get(Uri.parse(
+          'https://newsapi.org/v2/top-headlines?country=id&apiKey=029d5bef40ba4427a3ccb89b1f2ba0e1'));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          _posts = data['articles'];
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 }
